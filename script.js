8fa1ffeda9e899d1481454722fd0cd1b49bb7fe6
riki <ricardosk8532@gmail.com>
2026-05-06 16:05:48 -0300

    Subiendo Proyecto SCAW Clases 1 a 5

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Aplicación de resumen inicializada.");
+    // Aquí podremos agregar lógica interactiva (ej. acordeones, filtros, cuestionarios) a medida que agreguemos el contenido.
+});
